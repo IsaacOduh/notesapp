c32bcca433c497f9a995369ae1196a0c24ae359b
Isaac Oduh <ufuomaoduh@gmail.com>
2018-07-24 19:00:45 +0100

    setup the test files

---
+++ server/test/index.spec.js
@@ -0,0 +1,19 @@
+/* eslint-env mocha*/
+const assert = require('assert');
+const calculator = require('../index');
+
+describe('calculator',() => {
+    describe('add function',() => {
+        it('adds numbers', () => {
+            const result = calculator.add(1,1);
+            assert.equal(result,2);
+        });
+    });
+
+    describe('add function', () => {
+        it('multiplies numbers',() => {
+            const result = calculator.multiply(1,2);
+            assert.equal(result,2);
+        });
+    });
+});
