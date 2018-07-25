c26d921bc02a7efa96fb13f261bf3f3c8dfd002e
Isaac Oduh <ufuomaoduh@gmail.com>
2018-07-25 05:51:51 +0100

    entry points complete

--- server/test/index.spec.js
+++ server/test/index.spec.js
@@ -1,26 +1,26 @@
 /* eslint-env mocha*/
-const assert = require('assert');
-const calculator = require('../index');
+// const assert = require('assert');
+// const calculator = require('../index');
 
-describe('calculator',() => {
-    describe('add function',() => {
-        it('adds numbers', () => {
-            const result = calculator.add(1,1);
-            assert.equal(result,2);
-        });
-    });
+// describe('calculator',() => {
+//     describe('add function',() => {
+//         it('adds numbers', () => {
+//             const result = calculator.add(1,1);
+//             assert.equal(result,2);
+//         });
+//     });
 
-    describe('multiply function', () => {
-        it('multiplies numbers',() => {
-            const result = calculator.multiply(1,2);
-            assert.equal(result,2);
-        });
-    });
+//     describe('multiply function', () => {
+//         it('multiplies numbers',() => {
+//             const result = calculator.multiply(1,2);
+//             assert.equal(result,2);
+//         });
+//     });
 
-    describe('subtract function', () => {
-        it('subtracts numbers', () => {
-            const result = calculator.subtract(2,1);
-            assert.equal(result,1);
-        });
-    });
-});
+//     describe('subtract function', () => {
+//         it('subtracts numbers', () => {
+//             const result = calculator.subtract(2,1);
+//             assert.equal(result,1);
+//         });
+//     });
+// });
